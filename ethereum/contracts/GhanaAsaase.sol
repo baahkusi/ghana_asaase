573f8f6719cdcf4fd9ebc09ed30a7d8e298bd918
Baah Kusi <49212833+baahkusi@users.noreply.github.com>
2023-07-03 11:30:05 +0000

    update

--- ethereum/contracts/GhanaAsaase.sol
+++ ethereum/contracts/GhanaAsaase.sol
@@ -4,59 +4,46 @@ pragma solidity ^0.8.9;
 contract GhanaAsaase {
     address admin;
     // stores owner for each address
-    mapping(string => address) public landOwner;
-    event NewLandOwner(string landAddress, address owner);
-    event ChangedLandOwner(
-        string landAddress,
-        address oldOwner,
-        address newOwner
-    );
-    error WrongLandOwner(address);
-    error ExistingLandOwner(address);
+    mapping(string => string) public landOwner;
+    event NewLandOwner(string landAddress, string owner);
+    event ChangedLandOwner(string landAddress, string oldOwner, string newOwner);
+    error ExistingLandOwner(string);
     error NoLandOwner();
-
     constructor(address admin_) {
         admin = admin_;
     }
-
-    modifier onlyOwner() {
-        require(msg.sender == admin, "Only admin can make a call");
+    modifier onlyOwner {
+        require( msg.sender == admin, "Only admin can make a call");
         _;
     }
-
-    /**
-     * Assign a land to an owner
-     * can only be called by admin
-     */
-    function assignOwnership(
-        string calldata landAddress,
-        address owner
-    ) public returns (bool) {
-        if (landOwner[landAddress] != address(0)) {
+    modifier noLandOwner(string calldata landAddress) {
+        if (bytes(landOwner[landAddress]).length > 0) {
             revert ExistingLandOwner(landOwner[landAddress]);
         }
+        _;
+    }
+    modifier hasLandOwner(string calldata landAddress) {
+        if (bytes(landOwner[landAddress]).length == 0) {
+            revert NoLandOwner();
+        }
+        _;
+    }
+    /**
+    * Assign a land to an owner
+    * can only be called by admin
+     */
+    function assignOwnership(string calldata landAddress, string calldata owner) public onlyOwner noLandOwner(landAddress) returns(bool) {        
         landOwner[landAddress] = owner;
         emit NewLandOwner(landAddress, owner);
         return true;
     }
-
     /**
-     * change owner of land to new owner
-     * can only be called by admin
+    * change owner of land to new owner
+    * can only be called by admin
      */
-    function changeOwnership(
-        string calldata landAddress,
-        address oldOwner,
-        address newOwner
-    ) public returns (bool) {
-        if (landOwner[landAddress] == address(0)) {
-            revert NoLandOwner();
-        }
-        if (landOwner[landAddress] != oldOwner) {
-            revert WrongLandOwner(landOwner[landAddress]);
-        }
+    function changeOwnership(string calldata landAddress, string calldata newOwner) public onlyOwner hasLandOwner(landAddress) returns(bool) {
+        emit ChangedLandOwner(landAddress, landOwner[landAddress], newOwner);
         landOwner[landAddress] = newOwner;
-        emit ChangedLandOwner(landAddress, oldOwner, newOwner);
         return true;
     }
 }
